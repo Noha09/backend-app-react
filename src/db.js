e2e5a7c48f57c62cde5dcfb954751bebbf66ff19
Noe Ramirez <1210198@alumno.um.edu.mx>
2024-09-19 17:49:52 -0600

    fix: otro intento de conectar la DB

--- src/db.js
+++ src/db.js
@@ -1,12 +1,11 @@
-import cliente from 'pg';
-const { Pool } = cliente;
+import {config} from 'dotenv'
+import {pg} from 'pg';
 
-const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_DATABASE || 'clase_react',
-  password: process.env.DB_PASSWORD || '12345678',
-  port: process.env.DB_PORT || 5432,
+config()
+
+const pool = new pg.Pool({
+  connectionString: process.env.DB_USER,
+  ssl: true
 });
 
 export {
