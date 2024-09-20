ba8c3df66b5beec77db89271311c189ba60c3fd9
Noe Ramirez <1210198@alumno.um.edu.mx>
2024-09-19 18:19:20 -0600

    buld: cachar error de conexion de la db

--- src/db.js
+++ src/db.js
@@ -1,12 +1,18 @@
-import {config} from 'dotenv'
-import {pg} from 'pg';
+import cliente from 'pg';
+const { Pool } = cliente;
 
-config()
-
-const pool = new pg.Pool({
-  connectionString: process.env.DB_USER,
-  ssl: true
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
 });
+
+pool.connect()
+  .then(() => console.log('Conectado a la base de datos exitosamente ✔️'))
+  .catch(err => console.error('Error de conexión a la base de datos ❌', err.stack))
+  .finally(() => pool.end());
 
 export {
   pool
