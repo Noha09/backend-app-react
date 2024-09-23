995fda39ab546f598ce82ff44dc3e41943073aec
Noe Ramirez <1210198@alumno.um.edu.mx>
2024-09-23 16:23:59 -0600

    fix: configuracion base de datos

--- src/db.js
+++ src/db.js
@@ -1,19 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './src/.env' });
 import cliente from 'pg';
 const { Pool } = cliente;
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST, 
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
 
-pool.connect()
-  .then(() => console.log('Conectado a la base de datos exitosamente ✔️'))
-  .catch(err => console.error('Error de conexión a la base de datos ❌', err.stack))
-  .finally(() => pool.end());
 
-export {
-  pool
-};
+pool.connect()
+  .then(() => console.log('Conexión a la base de datos exitosa'))
+  .catch(err => console.error('Error de conexión a la base de datos:', err));
+
+export { pool };
