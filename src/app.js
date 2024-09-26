bf565086c3a7976f231d4431322efb66445b1410
Noe Ramirez <1210198@alumno.um.edu.mx>
2024-09-26 16:44:41 -0600

    fix: path para conectar el front

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors({
     origin: [
         // 'http://localhost:3000'
-        'https://backend-de-la-app-react.onrender.com'
+        'https://frontend-de-la-app-react.onrender.com'
     ],
     credentials: true
 }));
